343ccfa30a40cc54f1c13ac02161aa617eb1dfa9
Emir Kovacevic <emirkovacevic92@gmail.com>
2019-02-21 22:49:01 +1000

    added: Dealer update, delete features

--- Inventory Managment/Inventory Managment/Dealer_info.xaml.cs
+++ Inventory Managment/Inventory Managment/Dealer_info.xaml.cs
@@ -22,7 +22,7 @@ namespace Inventory_Managment
     public partial class Dealer_info : Window
     {
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Emir\Source\Repos\Inventory Managment\Inventory Managment\Inventory Managment\Inventory.mdf;Integrated Security=True");
-
+        DataRowView id = null;
         public Dealer_info()
         {
             InitializeComponent();
@@ -112,6 +112,113 @@ namespace Inventory_Managment
             }
         }
 
-        //UPDATE HANDLER
+        private void btn_dealer_delete_Click(object sender, RoutedEventArgs e)
+        {
+            //Check if dealer is selected
+            if (id == null)
+            {
+                MessageBox.Show("Dealer is not selected");
+                return;
+            }
+            else
+            {
+                //Convert row data array item to int 32
+                int id2 = Convert.ToInt32(id.Row.ItemArray[0]);
+
+                //Create SQL query command to delete user
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "delete from dealer_info where id=" + id2 + "";
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show(id.Row.ItemArray[1].ToString() + " \n from " + id.Row.ItemArray[1].ToString() + " company is removed !");
+                Display();
+                id = null;
+            }
+        }
+
+        //ROW CLICK
+        private void dataGrid_dealers_row_Click(object sender, MouseButtonEventArgs e)
+        {
+            //Get selected row data, as dataRowView type
+            id = dataGrid_dealers.SelectedItem as DataRowView;
+        }
+
+        //UPDATE SELECTED BUTTON
+        private void btn_dealer_update_Click(object sender, RoutedEventArgs e)
+        {
+            if (id == null)
+            {
+                MessageBox.Show("Dealer is not selected");
+                return;
+            }
+            else
+            {
+                //make update panel panel
+                grid_update_dealer.Visibility = Visibility.Visible;
+
+                //Fetch data from selected and input in text boxes
+                input_add_dealer_name_upd.Text = id.Row.ItemArray[1].ToString();
+                input_add_dealer_company_upd.Text = id.Row.ItemArray[2].ToString();
+                input_add_dealer_contact_upd.Text = id.Row.ItemArray[3].ToString();
+                input_add_dealer_address_upd.Text = id.Row.ItemArray[4].ToString();
+                input_add_dealer_city_upd.Text = id.Row.ItemArray[5].ToString();
+
+            }
+        }
+
+        //UPDATE BUTTON
+        private void btn_upd_dealer_final_Click(object sender, RoutedEventArgs e)
+        {
+            //VERIFICATION
+            if (input_add_dealer_name_upd.Text == "" || input_add_dealer_address_upd.Text == "" || input_add_dealer_city_upd.Text == "" || input_add_dealer_contact_upd.Text == "" || input_add_dealer_company_upd.Text == "")
+            {
+                MessageBox.Show("Dealers: name,company,address,city and contact \n fields cannot be empty!");
+                return;
+            }
+            else
+            {
+                //fetch from db where dealer name and company and put into dt3
+                //int counts;
+                string q1 = "select * from dealer_info where dealer_name='" + input_add_dealer_name_upd.Text + "' and dealer_company_name='" + input_add_dealer_company_upd.Text + "'";
+                DataTable dt3 = FetchProducts(q1);
+                //counts = dt3.Rows.Count;
+
+
+                //check if dealer exists
+                //if (counts == 0)
+                //{
+                //    MessageBox.Show("Cant update non-existing dealer");
+                //    return;
+                //}
+
+                //Convert row data array item to int 32
+                int id1 = Convert.ToInt32(id.Row.ItemArray[0]);
+
+                //else update
+                SqlCommand cmd1 = con.CreateCommand();
+                cmd1.CommandType = CommandType.Text;
+                cmd1.CommandText = "update dealer_info set dealer_name='" + input_add_dealer_name_upd.Text + "',dealer_company_name='" + input_add_dealer_company_upd.Text +
+                    "',contact='" + input_add_dealer_contact_upd.Text + "',address='" + input_add_dealer_address_upd.Text + "',city='" + input_add_dealer_city_upd.Text +
+                    "' where id=" + id1 + "";
+
+                cmd1.ExecuteNonQuery();
+
+                //Clear ID
+                id = null;
+
+                //Clear input box and message
+                MessageBox.Show(input_add_dealer_name_upd.Text + " has been updated successfully!");
+                input_add_dealer_name_upd.Text = "";
+                input_add_dealer_city_upd.Text = "";
+                input_add_dealer_address_upd.Text = "";
+                input_add_dealer_contact_upd.Text = "";
+                input_add_dealer_company_upd.Text = "";
+
+                grid_update_dealer.Visibility = Visibility.Collapsed;
+
+                Display();
+            }
+        }
     }
 }
